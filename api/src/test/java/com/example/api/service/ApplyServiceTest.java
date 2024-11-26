fc9ce44ba80f762cb94f8c1c1d0e1e0dbae1ffa5
Gukin Han <gukin.dev@gmail.com>
2024-11-26 21:33:25 +0900

    문제점 end

--- api/src/test/java/com/example/api/service/ApplyServiceTest.java
+++ api/src/test/java/com/example/api/service/ApplyServiceTest.java
@@ -6,6 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -23,6 +27,27 @@ class ApplyServiceTest {
 
         final long count = couponRepository.count();
         Assertions.assertThat(count).isEqualTo(1);
+    }
+
+    @Test
+    public void 여러명응모() throws InterruptedException {
+        int threadCount = 1000;
+        final ExecutorService executorService = Executors.newFixedThreadPool(32);
+        final CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            long userId = i;
+            executorService.submit(() -> {
+                try {
+                    applyService.apply(userId);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+        final long count = couponRepository.count();
+        Assertions.assertThat(count).isEqualTo(100);
     }
 
 }
